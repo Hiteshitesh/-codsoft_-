34a76589be8b84d5b96f020f1bf3a6b5332d05cc
Hiteshitesh <108985731+Hiteshitesh@users.noreply.github.com>
2024-04-30 00:03:35 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,27 @@
+let display = document.getElementById('display');
+let currentInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    display.value = currentInput;
+}
+
+function appendOperator(operator) {
+    currentInput += ' ' + operator + ' ';
+    display.value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        let result = eval(currentInput);
+        display.value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
